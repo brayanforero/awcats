083a98821c5dcef159473385689dcf2edbb2c852
Brayan Forero <bforero.engineer@gmail.com>
2023-05-23 00:30:10 -0400

    feature(root) add loading home page and pulse animation

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageResponse } from '@core/types';
+import { BreedService } from '@shared/services/breed.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  loading: boolean = true;
+  currentImage: ImageResponse | null = null;
 
+  constructor(private breedSvc: BreedService) {}
+
+  ngOnInit(): void {
+    this.breedSvc.getRandomImage().subscribe((images) => {
+      this.currentImage = images.at(0) as ImageResponse;
+      this.loading = false;
+    });
+  }
 }
